9f0f6b8dfaaf455b129e2060c225b002a8dd4e40
Inna Shvanova <igs2003@mail.ru>
2024-11-04 23:19:57 +0300

    r

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-S
+$(function(){
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
+});
